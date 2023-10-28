805834554787c7373d6792ea3c4c3e5a16b824f5
Victor M. Rodriguez <victordoblezz@gmail.com>
2023-10-27 20:10:23 -0400

    add player tests in the castr.io

--- src/components/player-multimedia/PlayerComponent.tsx
+++ src/components/player-multimedia/PlayerComponent.tsx
@@ -1,48 +1,55 @@
-import { styled } from '@mui/material'
-import React from 'react'
+import { styled } from '@mui/material';
+import React from 'react';
 
 interface PlayerProps {
-    // ...
+  // ...
 }
 
 const PlayerComponent: React.FC<PlayerProps> = () => {
-    return (
-        <Player>
-            <div className='video-wrapper'>
-                <video id="rtmpPlayer" controls>
-                    <source src="rtmp://tu-url-del-servidor-tu-stream" type="video/rtmp" />
-                    Tu navegador no admite la reproducción de video RTMP.
-                </video>
-            </div>
-        </Player>
-    )
-}
+  const iframeAttributes = {
+    src: "https://player.castr.com/live_4cd8ab70752211ee9862f9c11f992c1d",
+    width: "100%",
+    style: { aspectRatio: '16/9', minHeight: '340px' },
+    frameBorder: "0",
+    scrolling: "no",
+    allow: "autoplay",
+    allowFullScreen: true,
+    webkitallowfullscreen: "true",
+    mozallowfullscreen: "true",
+    oallowfullscreen: "true",
+    msallowfullscreen: "true",
+  };
 
-export default PlayerComponent
+  return (
+    <Player>
+      <iframe {...iframeAttributes}></iframe>
+    </Player>
+  );
+};
+
+export default PlayerComponent;
 
 const Player = styled('div')`
-    width: calc(100% - 10px);
-    max-width: 640px; /* Ancho máximo del contenedor del reproductor */
-    border-radius:10px;
-    overflow: hidden;
-    box-shadow: rgb(38, 57, 77) 0px 20px 30px -10px;
+  width: calc(100% - 10px);
+  max-width: 640px; /* Ancho máximo del contenedor del reproductor */
+  border-radius: 10px;
+  overflow: hidden;
+  box-shadow: rgb(38, 57, 77) 0px 20px 30px -10px;
+  position: relative;
+  margin-left: 50%;
+  transform: translateX(-50%);
+  background-color: #000;
+
+  .video-wrapper {
     position: relative;
-    margin-left:50%;
-    transform: translateX(-50%);
-    background-color:#000;
- 
-    .video-wrapper{
-        position: relative;
-        width: 100%;
-        padding-bottom: 56.25%; /* Misma proporción de aspecto para el video */
-        margin:auto;
-    
-    }
+    width: 100%;
+    padding-bottom: 56.25%; /* Misma proporción de aspecto para el video */
+    margin: auto;
+  }
 
-    video{
-        position: absolute;
-        width: 100%;
-        height: 100%;
-    }
-
-`
+  video {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
+`;
