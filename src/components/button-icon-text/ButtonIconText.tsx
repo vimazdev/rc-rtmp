557114e2f86b4ed661b64b60bd995a6609e22038
Victor M. Rodriguez <victordoblezz@gmail.com>
2023-11-11 11:15:46 -0400

    Change links Social and Fix Text

--- src/components/button-icon-text/ButtonIconText.tsx
+++ src/components/button-icon-text/ButtonIconText.tsx
@@ -9,21 +9,33 @@ interface SocialButtonProps {
   backgroundColor?: string;
   text?: string;
   icon?: JSX.Element; // Icono en formato JSX.Element
+  url?: string;
 }
 
 
 const ButtonIconText: React.FC<SocialButtonProps> = (props) => {
-  const { text, icon } = props;
+  const { text, icon, url } = props;
 
+  if (url) {
+    // Si se proporciona una URL, utiliza un enlace
+    return (
+      <a href={url} target='_blank' className="button-link">
+        <IconButton size="large" className={text}>
+          {icon}
+          {text}
+        </IconButton>
+      </a>
+    );
+  }
+
+  // Si no se proporciona una URL, simplemente renderiza el botón
   return (
-    <IconButton
-      size="large"
-      className={text}
-    >
+    <IconButton size="large" className={text}>
       {icon}
       {text}
     </IconButton>
   );
 };
+
 
 export default ButtonIconText;
